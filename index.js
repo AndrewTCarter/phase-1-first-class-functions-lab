6605e4b5e6d3515f9c8c9b15cf37aa152b312f02
AndrewTCarter <Andrewkarter@gmail.com>
2022-05-09 17:33:28 -0400

    commit tree 7c6b37b086c90585d4adacd0d6c678fcdcc2d0d9

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    let newArray = [drivers[0],  drivers[1]]
+    return newArray
+}
+
+const returnLastTwoDrivers = function(drivers){
+    let nextArray = [drivers[2], drivers[3]]
+    return nextArray
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function(fare){
+        return fare * int
+    }
+}
+
+function fareDoubler(fare){
+    const double = createFareMultiplier(2)
+    return (double(fare))
+}
+
+function fareTripler(fare){
+    const triple = createFareMultiplier(3)
+    return (triple(fare))
+}
+
+function selectDifferentDrivers(drivers,cb){
+    return cb(drivers)
+}
